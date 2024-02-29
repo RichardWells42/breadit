96dffdc0ba431b22de74e3ad977cd2d67d61d271
Richard <richardwells475@gmail.com>
2024-02-29 15:52:19 -0500

    trying to fix startsWith error 2

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -4,10 +4,8 @@ export async function GET(req:Request) {
   const url = new URL(req.url)
   const q = url.searchParams.get('q')
 
-  if (!q) {
-    return new Response('Invalid query', { status: 400 })
-  }
-  
+  if (!q) return new Response('Invalid query', { status: 400 })
+
   const results = await db.subreddit.findMany({
     where: {
         name: {
