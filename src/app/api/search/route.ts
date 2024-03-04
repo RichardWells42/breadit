d5addea080dc828689431ce69fc3101ef012c778
Richard <richardwells475@gmail.com>
2024-03-04 15:31:54 -0500

    added a space between req: and Request in route.ts...\search file

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,6 +1,6 @@
 import { db } from "@/lib/db"
 
-export async function GET(req:Request) {
+export async function GET(req: Request) {
   const url = new URL(req.url)
   const q = url.searchParams.get('q')
 
