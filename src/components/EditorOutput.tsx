7e8af2f979e167fa9acebdfff9f121fe185e3000
Richard <richardwells475@gmail.com>
2024-03-07 15:29:21 -0500

    added (data) to CustomCodeRenderer in EditorOutput.tsx

--- src/components/EditorOutput.tsx
+++ src/components/EditorOutput.tsx
@@ -20,8 +20,6 @@ const style = {
     },
   }
 
-
-
 const renderers = {
   image: CustomImageRenderer,
   code: CustomCodeRenderer,
@@ -40,6 +38,8 @@ const EditorOutput: FC<EditorOutputProps> = ({ content }) => {
 }
 
 function CustomCodeRenderer({ data }: any) {
+    (data)
+
     return (
       <pre className='bg-gray-800 rounded-md p-4'>
         <code className='text-gray-100 text-sm'>{data.code}</code>
