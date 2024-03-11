a17e20007d494506858b757176b8159bfa5907c2
Richard <richardwells475@gmail.com>
2024-03-11 15:37:42 -0400

    added middleware.ts file

--- src/components/MiniCreatePost.tsx
+++ src/components/MiniCreatePost.tsx
@@ -3,7 +3,7 @@
 import { Session } from "next-auth"
 import { usePathname, useRouter } from "next/navigation"
 import { FC } from "react"
-import UserAvatar from "./UserAvatar"
+import  UserAvatar from "./UserAvatar"
 import { Input } from "./ui/Input"
 import { Button } from "./ui/Button"
 import { ImageIcon, Link2 } from "lucide-react"
@@ -25,7 +25,7 @@ const MiniCreatePost: FC<MiniCreatePostProps> = ({ session }) => {
                 // @ts-ignore
                 name: session?.user.name || null,
                 image: session?.user?.image || null,
-            }} 
+              }} 
             />
 
             <span className='absolute bottom-0 right-0 rounded-full w-3 h-3 bg-green-500 outline outline-2 outline-white' />
